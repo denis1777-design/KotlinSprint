d7add9bf09937ee492dba2a947318363914d1836
Denis <denisg7@outlook.com>
2024-09-05 08:39:08 +0300

    KotlinSprint-1-1 create task1

--- src/main/kotlin/Lesson_1/lesson1_task1.kt
+++ src/main/kotlin/Lesson_1/lesson1_task1.kt
@@ -3,10 +3,10 @@ package org.example.Lesson_1
 fun main() {
     println("решение задачи 1 к Уроку 1")
 
-    var numberOfOrder: Int = 75
+    var numberOfOrder: Int = 75  // кол-во заказов указал ключ.словом var как изменяемую переменную. В разные дни кол-во заказов может меняться
     println("Количество заказов интернет-магазина: $numberOfOrder")
 
-    var notificationText: String = "Спасибо, что приобрели наш продукт!"
+    val notificationText: String = "Спасибо, что приобрели наш продукт!"
     println(notificationText)
 
 
