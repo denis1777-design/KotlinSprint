e22852492a0b0b6352581c8dacb421f5fcdbeb94
Denis <denisg7@outlook.com>
2024-09-05 19:00:49 +0300

    KotlinSprint-1-4 create task4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,24 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    //println("Hello World!")
+    var notificationText: String ="Маски и перчатки не потребуются!"
+    println(notificationText)
+
+    val intNum1 = -2147483648 //32 bit
+    val intNum2 = -2147483648
+    val longNum: Long// 64 bit
+    val shortNum: Short//16 bit
+    val byteNun:Byte // 8 bit
+    val unsignedNum: UInt = 214748648u //32 bit
+
+    val doubleNum: Double = 23.56 // 64 bit
+    val floatNum: Float = 4.5f // 32 bit
+
+    // String
+    val stringValue : String = "Это строка"
+
+    // Logic
+    val trueValue: Boolean = true
+    val falseValue:Boolean = false
 }
