775cb940f3e4fb9dce9f19ede712c69c6949c9cd
Denis <denisg7@outlook.com>
2024-09-05 08:34:04 +0300

    KotlinSprint-1-1 create task1

---
+++ src/main/kotlin/Lesson_1/lesson1_task1.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson_1
+
+fun main() {
+    println("решение задачи 1 к Уроку 1")
+
+    var numberOfOrder: Int = 75
+    println("Количество заказов интернет-магазина: $numberOfOrder")
+
+    var notificationText: String = "Спасибо, что приобрели наш продукт!"
+    println(notificationText)
+
+
+
+
+
+}
