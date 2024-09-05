93cc672aa453edebc8a8b9fc672c0f6863b13c43
Denis <denisg7@outlook.com>
2024-09-05 18:55:55 +0300

    KotlinSprint-1-4 create task4

---
+++ src/main/kotlin/Lesson_4/KS-1-4.kt
@@ -0,0 +1,25 @@
+package org.example.Lesson_4
+
+fun main() {
+  /*  Данные для задачи:
+    Расстояние 40_868_600_000
+    Возраст – 27
+    Часть дня – 0.075
+    Секунды – 6480
+    Часть года – 2.0547945205479453E-4
+    Апогей орбиты – 327000*/
+
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453e-4
+    val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
