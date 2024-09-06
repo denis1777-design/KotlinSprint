13136c2689a8130aac0f6617a5bdb0dbaf534a9b
Denis <denisg7@outlook.com>
2024-09-06 18:10:29 +0300

    KS-2-1 edit middle arithmetics

--- src/main/kotlin/Lesson_2/KS-2-1.kt
+++ src/main/kotlin/Lesson_2/KS-2-1.kt
@@ -1,5 +1,10 @@
 package org.example.Lesson_2
 
 fun main() {
-    println(String.format("%.2f", (((3 + 4 + 3 + 5) / 4).toFloat())))
+    val man1Ball: Byte = 3
+    val man2Ball: Byte = 4
+    val man3Ball: Byte = 3
+    val man4Ball: Byte = 5
+
+    println(String.format("%.2f", (((man1Ball + man2Ball + man3Ball + man4Ball) / 4).toFloat())))
 }
