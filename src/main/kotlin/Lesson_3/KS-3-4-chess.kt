ba9005c31612c789192a2ba233efa4a83ecef98b
Denis <denisg7@outlook.com>
2024-09-07 17:10:52 +0300

    KS-3-4 create Shess task 4

---
+++ src/main/kotlin/Lesson_3/KS-3-4-chess.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson_3
+
+fun main() {
+    var initPosition: String = "E2"
+    var nextPosition: String = "E4"
+    var motionChess: String = "1"
+
+    println("$initPosition - $nextPosition: $motionChess")
+
+    initPosition = "D2"
+    nextPosition = "D3"
+    motionChess = "2"
+
+    println("$initPosition - $nextPosition: $motionChess")
+}
