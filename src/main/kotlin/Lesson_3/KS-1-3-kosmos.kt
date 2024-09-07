022ba4707da652b70da8c284bb8d76d0465c7684
Denis <denisg7@outlook.com>
2024-09-07 07:13:10 +0300

    KS-1-3 edit task 3

--- src/main/kotlin/Lesson_3/KS-1-3-kosmos.kt
+++ src/main/kotlin/Lesson_3/KS-1-3-kosmos.kt
@@ -1,18 +1,16 @@
 package org.example.Lesson_3
 
 fun main() {
-    val year: Int = 1961
-    var hour: Byte = 9
-    var minute: Byte = 7
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
 
-    println("---Взлет---")
-    println("Год полета – $year")
-    println("%02d".format(hour))
-    println("%02d".format(minute))
+    println("Год полета: $year")
+    println(hour)
+    println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
-    println("---Посадка---")
     println("$hour:$minute")
 }
