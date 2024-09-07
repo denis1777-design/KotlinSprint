062ad5b3f94c49985bc8ecd092087d0a8272d63d
Denis <denisg7@outlook.com>
2024-09-07 15:02:36 +0300

    KS-3-2 create getting married

---
+++ src/main/kotlin/Lesson_3/KS-3-2-getting married.kt
@@ -0,0 +1,12 @@
+package org.example.Lesson_3
+
+fun main() {
+    val name: String = "Татьяна Сергеевна"
+    val untilMarry: String = "Андреева"
+    val getMarry: String = "Сидорова"
+    val untilAge: String = "20"
+    val age: String = "22"
+
+    println("$untilMarry $name, $untilAge")
+    println("$getMarry $name, $age")
+}
