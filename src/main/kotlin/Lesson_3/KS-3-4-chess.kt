c8f04eb6182b9a75d8f80707dc94ad565a0f0849
Denis.Gontsov <denisgon@gmail.com>
2024-09-08 15:49:11 +0300

    KS-3-4-chess edit

--- src/main/kotlin/Lesson_3/KS-3-4-chess.kt
+++ src/main/kotlin/Lesson_3/KS-3-4-chess.kt
@@ -5,11 +5,11 @@ fun main() {
     var nextPosition: String = "E4"
     var motionChess: String = "1"
 
-    println("$initPosition - $nextPosition: $motionChess")
+    println("$initPosition-$nextPosition:$motionChess")
 
     initPosition = "D2"
     nextPosition = "D3"
     motionChess = "2"
 
-    println("$initPosition - $nextPosition: $motionChess")
+    println("$initPosition-$nextPosition:$motionChess")
 }
