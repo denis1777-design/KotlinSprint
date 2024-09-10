6d9501d84164bc403740f2654f8e7bf47560f001
Denis <denisg7@outlook.com>
2024-09-10 17:46:25 +0300

    KS-4-2 edit

--- src/main/kotlin/Less_4/KS-4-2-griz-na-tamojne.kt
+++ src/main/kotlin/Less_4/KS-4-2-griz-na-tamojne.kt
@@ -6,8 +6,22 @@ fun main() {
     val weight2: Byte = 50
     val volume2: Byte = 100
 
-    println("Груз с весом $weight1 кг и объемом $volume1 л соответствует категории 'Average': ${(weight1 > WEIGHT_KG_MIN) && (weight1 <= WEIGHT_KG_MAX) && (volume1 < VOLUME_L)}")
-    println("Груз с весом $weight2 кг и объемом $volume2 л соответствует категории 'Average': ${(weight2 > WEIGHT_KG_MIN) && (weight2 <= WEIGHT_KG_MAX) && (volume2 < VOLUME_L)}")
+    println(
+        "Груз с весом $weight1 кг и объемом $volume1 л соответствует категории 'Average': " +
+                "${
+                    (weight1 > WEIGHT_KG_MIN) &&
+                            (weight1 <= WEIGHT_KG_MAX) &&
+                            (volume1 < VOLUME_L)
+                }"
+    )
+    println(
+        "Груз с весом $weight2 кг и объемом $volume2 л соответствует категории 'Average': " +
+                "${
+                    (weight2 > WEIGHT_KG_MIN) &&
+                            (weight2 <= WEIGHT_KG_MAX) &&
+                            (volume2 < VOLUME_L)
+                }"
+    )
 }
 
 const val WEIGHT_KG_MIN = 35
