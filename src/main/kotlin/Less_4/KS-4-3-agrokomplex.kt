b1ec945f252ea452ede2d31009adef9c60210f90
Denis <denisg7@outlook.com>
2024-09-10 17:34:18 +0300

    KS-4-3 edit agrokomplex

--- src/main/kotlin/Less_4/KS-4-3-agrokomplex.kt
+++ src/main/kotlin/Less_4/KS-4-3-agrokomplex.kt
@@ -1,15 +1,18 @@
 package org.example.Less_4
 
 fun main() {
-    val weather: Boolean = true
+    val isSunny: Boolean = true
     val tent: Boolean = true
     val humidity: Int = 20
     val timeOfYear: String = "зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${(weather == WEATHER_TODAY) && (tent == TENT) && (humidity == HUMIDITY) && (timeOfYear == TIME_OF_YEAR)}")
+    println("Благоприятные ли условия сейчас для роста бобовых? ${(isSunny == WEATHER_TODAY) &&
+            (tent == TENT) && (humidity == HUMIDITY) &&
+            timeOfYear != TIME_OF_YEAR
+    }")
 }
 
 const val WEATHER_TODAY: Boolean = true
 const val TENT: Boolean = true
 const val HUMIDITY: Int = 20
-const val TIME_OF_YEAR: String = "не зима"
+const val TIME_OF_YEAR: String = "зима"
