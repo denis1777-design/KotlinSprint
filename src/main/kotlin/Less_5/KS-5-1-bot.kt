e7d84e76fabeb9e8cdaa65073142146d9b15fd36
Denis <denisg7@outlook.com>
2024-09-12 12:13:36 +0300

    KS-5-1 are you bot

---
+++ src/main/kotlin/Less_5/KS-5-1-bot.kt
@@ -0,0 +1,10 @@
+package org.example.Less_5
+
+fun main() {
+    print("Сколько будет 328 + 107 = ")
+
+    val numberChecked: Int = readln().toInt()
+
+    if (numberChecked == 435) println("Добро пожаловать!")
+    else println("Доступ запрещен!")
+}
