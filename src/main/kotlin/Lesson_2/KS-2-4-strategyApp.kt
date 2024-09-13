c730676fb872512e09b2cf731c95b8d65c11b153
Denis <denisg7@outlook.com>
2024-09-13 19:27:42 +0300

    KS-2-4 edit srategy App

--- src/main/kotlin/Lesson_2/KS-2-4-strategyApp.kt
+++ src/main/kotlin/Lesson_2/KS-2-4-strategyApp.kt
@@ -8,7 +8,13 @@ fun main() {
 
     val crystallineOre: Byte = 7
 
-    println("Bonus baff (iron): ${(ironOre * 1.2 - ironOre).roundToInt()}")
+    val procent: Int = 20
 
-    println("Bonus baff (crystalline): ${(crystallineOre * 1.2 - crystallineOre).roundToInt()}")
+    println("Bonus baff (iron): ${(ironOre * (1 + (procent.toDouble().div(100))) - ironOre).roundToInt()}")
+
+    println(
+        "Bonus baff (crystalline): ${
+            (crystallineOre * (1 + (procent.toDouble().div(100))) - crystallineOre).roundToInt()
+        }"
+    )
 }
