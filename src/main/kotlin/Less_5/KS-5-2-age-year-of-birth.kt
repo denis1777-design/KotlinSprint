d713a0f18f92dc65f379f4be6f9bcf8a728883e6
Denis <denisg7@outlook.com>
2024-09-13 20:37:18 +0300

    KS-5-2 edit

--- src/main/kotlin/Less_5/KS-5-2-age-year-of-birth.kt
+++ src/main/kotlin/Less_5/KS-5-2-age-year-of-birth.kt
@@ -4,7 +4,7 @@ fun main() {
 
     var year: Int = 2024
 
-    val userAge = readln()!!.toInt()
+    val userAge = readln().toInt()
 
     if (year - userAge >= AGE_OF_MAJORITY) {
         println("Показать экран со скрытым контентом")
