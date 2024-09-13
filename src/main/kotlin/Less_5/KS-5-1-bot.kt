7fadf565ef79429fdf6edbf0d10915390f23b094
Denis <denisg7@outlook.com>
2024-09-13 06:36:12 +0300

    KS-5-1 edit Bot

--- src/main/kotlin/Less_5/KS-5-1-bot.kt
+++ src/main/kotlin/Less_5/KS-5-1-bot.kt
@@ -1,10 +1,13 @@
 package org.example.Less_5
 
 fun main() {
-    print("Сколько будет 328 + 107 = ")
 
-    val numberChecked: Int = readln().toInt()
+    val number1: Byte = 32
+    val number2: Byte = 10
+    val numberChecked: Byte = readln().toByte()
 
-    if (numberChecked == 435) println("Добро пожаловать!")
+    print("Сколько будет 32 + 10 = ")
+
+    if (numberChecked == (number1 + number2).toByte()) println("Добро пожаловать!")
     else println("Доступ запрещен!")
 }
